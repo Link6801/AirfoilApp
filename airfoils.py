c8746a8c83cd8d66a1cf23f2bdfd99bbffc2fdff
Amit Ghosh <amitghosh@Amits-MacBook-Pro-2.local>
2026-04-25 01:22:27 +0530

    standard naca 5 family working

--- airfoils.py
+++ airfoils.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import brentq
 
 st.set_page_config(page_title="FoilLabs - Geometry Simplified", page_icon="✈️",layout="wide")
 st.sidebar.header("Airfoil Parameters")
@@ -43,16 +44,45 @@ with st.sidebar:
             st.write("Reflex Camber?")
         with col2:
             ref = st.toggle(label="reflex", label_visibility="collapsed")
+        reflex=0
         if ref:
-            re=1
+            reflex=1
         else:
-            re=0
+            reflex=0
         t = st.sidebar.slider("Maximum Thickness (t)", 0.01, 0.3, 0.12, 0.01)
         if t>=0.10:
-            naca = f"NACA {int(l/0.15)}{int(p*20)}{re}{int(t*100)}"
+            naca = f"NACA {int(l/0.15)}{int(p*20)}{reflex}{int(t*100)}"
         else:
-            naca = f"NACA {int(l/0.15)}{int(p*20)}{re}0{int(t*100)}"
+            naca = f"NACA {int(l/0.15)}{int(p*20)}{reflex}0{int(t*100)}"
         st.sidebar.write(f"**Selected Airfoil:** {naca}")
+        if reflex==0:
+            def F(r):
+                return r*(1-(r/3)**0.5)-p
+            r1,r2,r=brentq(F,0,4/3,xtol=1e-12, rtol=1e-12, maxiter=1000),brentq(F,4/3,3,xtol=1e-12, rtol=1e-12, maxiter=1000),0
+            if r1>1:
+                r=r2
+            else:
+                r=r1
+            N=(3*r - 7*r**2 + 8*r**3 - 4*r**4) / np.sqrt(r * (1 - r))- (3/2) * (1 - 2*r) * (np.pi/2 - np.arcsin(1 - 2*r))
+            k1=(6*(0.15*int(l/0.15)))/N
+            x = 0.5 * (1 - np.cos(np.linspace(0, np.pi, sp)))
+            z = (t/0.2)*(0.2969*np.sqrt(x) - 0.1260*x - 0.3516*x**2 + 0.2843*x**3 - 0.1015*x**4)
+
+            yc=np.where(x<r,
+                        (k1 / 6) * (x**3 - 3*r*x**2 + r**2*(3 - r)*x),
+                        (k1 * r**3 / 6) * (1 - x))
+            dyc_dx=np.where(x<r,
+                            (k1 / 6) * (3*x**2 - 6*r*x + r**2*(3 - r)),
+                            - (k1 * r**3) / 6)
+            theta = np.arctan(dyc_dx)
+
+            xu = x - z*np.sin(theta)
+            yu = yc + z*np.cos(theta)
+            xl = x + z*np.sin(theta)
+            yl = yc - z*np.cos(theta)
+
+        elif reflex==1:
+            print()
 
         st.sidebar.markdown("Work in Progress")
         
